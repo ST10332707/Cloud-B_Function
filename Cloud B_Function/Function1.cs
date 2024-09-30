14ed347f6aba312ac882287ec20c7f84d2e00b21
Unathi <Unathikolberg17@gmail.com>
2024-09-30 22:19:49 +0200

    commit

--- Cloud B_Function/Function1.cs
+++ Cloud B_Function/Function1.cs
@@ -5,22 +5,22 @@ using Microsoft.Azure.WebJobs.Extensions.Storage.Queues;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
-    {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
+//var host = new HostBuilder()
+//    .ConfigureFunctionsWebApplication()
+//    .ConfigureServices(services =>
+//    {
+//        services.AddApplicationInsightsTelemetryWorkerService();
+//        services.ConfigureFunctionsApplicationInsights();
 
-    })
-    .ConfigureWebJobs(b =>
-    {
-        // Register specific storage bindings
-        b.AddHttp();
-        b.AddAzureStorageBlobs(); // For Blob Storage functions
-        b.AddAzureStorageQueues(); // For Queue Storage functions
-        // b.AddAzureStorageQueuesScaleForTrigger(); // Add this if scaling is needed for Queue Triggers
-    })
-    .Build();
+//    })
+//    .ConfigureWebJobs(b =>
+//    {
+//        // Register specific storage bindings
+//        b.AddHttp();
+//        b.AddAzureStorageBlobs(); // For Blob Storage functions
+//        b.AddAzureStorageQueues(); // For Queue Storage functions
+//        // b.AddAzureStorageQueuesScaleForTrigger(); // Add this if scaling is needed for Queue Triggers
+//    })
+//    .Build();
 
-host.Run();
+//host.Run();
